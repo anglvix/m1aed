dd6d0c70a2762d7ac59ad40b1ebf432ad768d621
anglvix <roserainealt@gmail.com>
2025-10-09 10:36:45 +0100

    ex 1 a 11

--- FICHA1/ex11.py
+++ FICHA1/ex11.py
@@ -1,22 +1,24 @@
 idade = int(input("Idade: "))
 
-if 0 <= idade <= 1:
-    etapa = "Primeira infância"
-elif 2 <= idade <= 4:
-    etapa = "Infância"
-elif 5 <= idade <= 11:
-    etapa = "Segunda infância"
-elif 12 <= idade <= 18:
-    etapa = "Adolescência"
-elif 19 <= idade <= 29:
-    etapa = "Adulto – Jovem adulto"
-elif 30 <= idade <= 59:
-    etapa = "Adulto – Meia idade"
-elif 60 <= idade <= 74:
-    etapa = "Idoso – Idade madura"
-elif 75 <= idade <= 90:
-    etapa = "Velhice"
-else:
-    etapa = "Longevidade (acima de 90 anos)"
+match idade:
+    case idade if 0 <= idade <= 1:
+        etapa = "Primeira infância"
+    case idade if 2 <= idade <= 4:
+        etapa = "Infância"
+    case idade if 5 <= idade <= 11:
+        etapa = "Segunda infância"
+    case idade if 12 <= idade <= 18:
+        etapa = "Adolescência"
+    case idade if 19 <= idade <= 29:
+        etapa = "Adulto – Jovem adulto"
+    case idade if 30 <= idade <= 59:
+        etapa = "Adulto – Meia idade"
+    case idade if 60 <= idade <= 74:
+        etapa = "Idoso – Idade madura"
+    case idade if 75 <= idade <= 90:
+        etapa = "Velhice"
+    case _:
+        etapa = "Longevidade (acima de 90 anos)"
 
 print(etapa)
+
